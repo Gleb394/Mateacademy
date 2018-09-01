2a350baab2d95a39d6af75e38f410f1928ad58ea
Gleb394 <Glebas3@gmail.com>
2018-09-01 14:05:43 +0300

    push

--- src/main/java/homeWork/TestArray.java
+++ src/main/java/homeWork/TestArray.java
@@ -5,14 +5,13 @@ package homeWork;
  */
 public class TestArray {
 
-    public void arrayRotation(int values[], int length ,int k){
+    public void arrayRotation(int values[], int length, int k) {
         /*Integer[] valuesCopy = new int[k];
         System.arraycopy(values, 0, valuesCopy, 0, k);
         System.arraycopy(values, k, values, 0, length-k);
         System.arraycopy(valuesCopy, 0, values, length-k, k);*/
 
         for (int i = 0; i < k; i++) {
-
             int valueMove = values[0];
             for (int j = 0; j < length - 1; ++j)
                 values[j] = values[j + 1];
@@ -23,14 +22,15 @@ public class TestArray {
         }
     }
 
-    public void arrayRestore(int values[], int length){
-        for(int i = 0; i < length - 1; ++i){
-
-            if(values[i] < 0){
-                if(values[i - 1] < values[i + 1] && values[i + 1] <= values[i + 2]){
+    public void arrayRestore(int values[], int length) {
+        for (int i = 0; i < length - 1; ++i) {
+            if (values[i] < 0) {
+                if (values[i - 1] < values[i + 1] && values[i + 1] <= values[i + 2]) {
                     values[i] = values[i - 1] + 1;
-                }else if(values[i - 1] > values[i + 1] && values[i + 1] >= values[i + 2]){
+                } else if (values[i - 1] > values[i + 1] && values[i + 1] >= values[i + 2]) {
                     values[i] = values[i - 1] - 1;
+                } else if (values[i - 1] == values[i + 1]) {
+                    values[i] = values[i + 1] - 1;
                 }
             }
         }
@@ -39,21 +39,22 @@ public class TestArray {
         }
     }
 
-    public void range(int values[], int length){
+    public void range(int values[], int length) {
         int arrayBlock[] = new int[length];
         int counter = 0;
 
-        for (int i = 0; i <= length - 1; ++i){
+        for (int i = 0; i <= length - 1; ++i) {
             arrayBlock[counter] = values[i];
             counter++;
             int size = i + 1;
 
-            if (size == length || values[i] + 1 != values[i +1]){
-                if (counter > 1){
+            if (size == length || values[i] + 1 != values[i + 1]) {
+                if (counter > 1) {
                     System.out.print("[" + getMaxValue(arrayBlock, counter) + " " + getMinValue(arrayBlock, counter) + "]");
                     counter = 0;
                     arrayBlock = new int[length];
-                }if (counter == 1){
+                }
+                if (counter == 1) {
                     System.out.print("[" + arrayBlock[0] + "]");
                     counter = 0;
                     arrayBlock = new int[length];
