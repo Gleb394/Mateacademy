94ad8fe54487cc562f39237e8ec95b0a52964b6c
Gleb394 <Glebas3@gmail.com>
2018-09-07 17:47:39 +0300

    Update HomeWork2

--- src/main/java/homeWork/HomeWork2.java
+++ src/main/java/homeWork/HomeWork2.java
@@ -3,11 +3,10 @@ package homeWork;
 public class HomeWork2 {
 
     public String palindrome(String word){
-        char[] array = word.toCharArray();
-        int backward = array.length - 1;
+        int backward = word.length() - 1;
 
         for (int i = 0; backward > i; i++) {
-            if(array[i] != array[backward]){
+            if(word.charAt(i) != word.charAt(backward)){
                 return "isn't palindrome";
             }
             backward--;
@@ -18,16 +17,14 @@ public class HomeWork2 {
     public void reverse(int i, int j, String letters){
         char quotes = '"';
         int counter = 0;
-        System.out.print(letters + " - substring ");
-        System.out.print(quotes);
+        System.out.print(letters + " - substring " + quotes);
         for (; i <= j; i++){
             System.out.print(letters.charAt(i));
             counter++;
         }
         i = i - counter;
         System.out.print(quotes);
-        System.out.print(" was reversed to ");
-        System.out.print(quotes);
+        System.out.print(" was reversed to " + quotes);
 
         for (; j >= i; j--){
             System.out.print(letters.charAt(j));
@@ -36,10 +33,9 @@ public class HomeWork2 {
     }
 
     public int frequency(char letter, String letters){
-        char[] array = letters.toCharArray();
         int counter = 0;
 
-        for (char chars: array) {
+        for (char chars: letters.toCharArray()) {
             if(letter == chars){
                 counter++;
             }
